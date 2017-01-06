ff37f65e191845ca6aa7de3cf88d59a7ac51f096
Aturn3d <Aturn3d@gmail.com>
2017-01-06 23:35:01 +0300

    App for local bot tests

---
+++ Bot.ApplicationForTests/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Core;
+using Bot.DAL.Infrastructure;
+using Bot.DAL.Repositories;
+using Bot.Services;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace Bot.ApplicationForTests
+{
+    class Program
+    {
+        static IContainer Container;
+        static ITelegramBotClient Bot;
+        static void ConfigureAutofac()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+            builder.RegisterType<DatabaseFactory>().As<IDatabaseFactory>().InstancePerLifetimeScope();
+            builder.RegisterType<BotFactory>().As<IBotFactory>().SingleInstance();
+            builder.RegisterAssemblyTypes(typeof(UserRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterAssemblyTypes(typeof(TelegramBotService).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            Container = builder.Build();
+        }
+        static void Main(string[] args)
+        {
+            ConfigureAutofac();
+            IBotFactory fac = new BotFactory();
+            Bot = fac.GetTelegramBot();
+           
+            Bot.OnUpdate += Bot_OnUpdate;
+            Bot.OnReceiveError += BotOnReceiveError;
+            var me = Bot.GetMeAsync().Result;
+
+
+            Console.WriteLine(me.Username);
+            Bot.StartReceiving();
+
+            Bot.StopReceiving();
+        }
+
+
+
+        private static async void Bot_OnUpdate(object sender, Telegram.Bot.Args.UpdateEventArgs e)
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var service = scope.Resolve<ITelegramBotService>();
+                await service.HandleUpdate(e.Update);
+            }
+        }
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Debugger.Break();
+        }
+    }
+}
