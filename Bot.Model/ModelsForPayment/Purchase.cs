301b7b1794044822077599c1f7f930cfd8a7961b
Stanislav <Stanislav_Berazouski@epam.com>
2017-01-08 21:00:46 +0300

    add pay purchase , ticket .

---
+++ Bot.Model/ModelsForPayment/Purchase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Model.ModelsForPayment
+{
+    public class Purchase
+    {
+        public Purchase(string name, decimal amount, int count)
+        {
+            this.Name = name;
+            this.Amount = amount;
+            this.Count = count;
+        }
+
+        public string Name { get; }
+
+        public decimal Amount { get; }
+
+        public int Count { get; }
+
+        public static Purchase GetPurchase(string code)
+        {
+            if (!string.IsNullOrEmpty(code) && code.Length == 4)
+            {
+                return new Purchase("Kettle", 33, 2);
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Purchase name :{Name}";
+        }
+    }
+}
